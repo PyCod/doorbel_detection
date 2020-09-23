8ef7b61a1205bde754e53a773b4b12e0ab3fa568
Victor Sonck <victor.sonck@ml6.eu>
2020-09-23 20:40:58 +0200

    Fixed multiple notifications per ring and added function to save recording when detected

--- bell_detector/main.py
+++ bell_detector/main.py
@@ -3,6 +3,7 @@ import os
 from queue import Queue
 from threading import Thread
 from time import time
+from datetime import date
 
 from pyAudioAnalysis import audioTrainTest as aT
 from pyAudioAnalysis import MidTermFeatures as aF
@@ -10,6 +11,10 @@ from pyAudioAnalysis import MidTermFeatures as aF
 from pushover import init, Client
 
 import sounddevice as sd
+import soundfile as sf
+
+from creds import app_token, client_token
+
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -21,59 +26,82 @@ class PushWorker(Thread):
         Thread.__init__(self)
         self.notif_queue = notif_queue
 
-        init('<application token here>')
-        self.members = [Client("<user token here>")] # Mine
+        init(app_token)
+        self.members = [Client(client_token)] # Mine
+
+        self.last_pinged = time()
 
     def run(self):
         while True:
             _ = self.notif_queue.get()
-            for client in self.members:
-                client.send_message("Deurbel!")
+            if time() - self.last_pinged > 5:
+                for client in self.members:
+                    client.send_message("Deurbel!")
+                self.last_pinged = time()
+            self.notif_queue.task_done()
+
+
+class SaveWorker(Thread):
+
+    def __init__(self, save_queue, location='triggers', sr=22050):
+        Thread.__init__(self)
+        self.save_queue = save_queue
+        self.location = location
+        self.sr = sr
+
+    def run(self):
+        while True:
+            recording = self.save_queue.get()
+            sf.write(os.path.join(self.location, f'{date.today()}.wav'),
+                     recording, self.sr, subtype='PCM_24')
+            self.save_queue.task_done()
 
 
 class RecordingWorker(Thread):
 
-    def __init__(self, bell_queue, seconds, fs):
+    def __init__(self, bell_queue, seconds, sr):
         Thread.__init__(self)
         self.bell_queue = bell_queue
         self.seconds = seconds
-        self.fs = fs
+        self.sr = sr
 
     def run(self):
         while True:
             # Get the work from the bell_queue and expand the tuple
-            recording = sd.rec(int(self.seconds * self.fs), samplerate=self.fs, channels=1)
+            recording = sd.rec(int(self.seconds * self.sr), samplerate=self.sr, channels=1)
             sd.wait()
-            self.bell_queue.put((self.fs, recording.reshape(recording.shape[0])))
+            self.bell_queue.put((self.sr, recording.reshape(recording.shape[0])))
 
 
 class DetectionWorker(Thread):
 
-    def __init__(self, bell_queue, notif_queue, model_name, model_type):
+    def __init__(self, bell_queue, notif_queue, save_queue,
+                 model_name, model_type):
         Thread.__init__(self)
         self.bell_queue = bell_queue
         self.notif_queue = notif_queue
+        self.save_queue = save_queue
         self.model_name = model_name
         self.model_type = model_type
 
 
-    def process_recording(self, signal, fs):
+    def process_recording(self, signal, sr):
         classifier, mean, std, classes, mid_window, mid_step, short_window, \
             short_step, compute_beat = aT.load_model(self.model_name)
 
-        if fs == 0:
+        if sr == 0:
             # audio file IO problem
             return -1, -1, -1
-        if signal.shape[0] / float(fs) < mid_window:
-            mid_window = signal.shape[0] / float(fs)
+        if signal.shape[0] / float(sr) < mid_window:
+            mid_window = signal.shape[0] / float(sr)
 
         # feature extraction:
         mid_features, s, _ = \
-            aF.mid_feature_extraction(signal, fs,
-                                    mid_window * fs,
-                                    mid_step * fs,
-                                    round(fs * short_window),
-                                    round(fs * short_step))
+            aF.mid_feature_extraction(signal, sr,
+                                    mid_window * sr,
+                                    mid_step * sr,
+                                    round(sr * short_window),
+                                    round(sr * short_step))
         # long term averaging of mid-term statistics
         mid_features = mid_features.mean(axis=1)
 
@@ -87,14 +115,15 @@ class DetectionWorker(Thread):
 
     def run(self):
         while True:
-            fs, recording = self.bell_queue.get()
+            sr, recording = self.bell_queue.get()
             start_detection = time()
             try:
-                class_out = self.process_recording(recording, fs)[0]
+                class_out = self.process_recording(recording, sr)[0]
                 logging.info(class_out)
                 if class_out == 0:
                     logging.info('Sending push notification to queue!')
                     self.notif_queue.put(True)
+                    self.save_queue.put(recording)
             except Exception as e:
                 print(e)
             finally:
@@ -108,9 +137,12 @@ def main():
     bell_queue = Queue()
     # Create a notif_queue to send push notifications
     notif_queue = Queue()
+    # Create a save_queue to save sounds for later reuse in training
+    save_queue = Queue()
 
     # Start processing thread first
-    det_worker = DetectionWorker(bell_queue, notif_queue, 'model/svm_bell', 'svm')
+    det_worker = DetectionWorker(bell_queue, notif_queue, save_queue,
+                                 'model/svm_bell', 'svm')
     # Setting daemon to True will let the main thread exit even though the workers are blocking
     det_worker.daemon = False
     det_worker.start()
@@ -126,6 +158,12 @@ def main():
     # Setting daemon to True will let the main thread exit even though the workers are blocking
     notif_worker.daemon = False
     notif_worker.start()
+
+    # Start the saving worker which will save all bell instances
+    save_worker = SaveWorker(save_queue)
+    # Setting daemon to True will let the main thread exit even though the workers are blocking
+    save_worker.daemon = False
+    save_worker.start()
 
     # Causes the main thread to wait for the bell_queue to finish processing all the tasks
     bell_queue.join()
